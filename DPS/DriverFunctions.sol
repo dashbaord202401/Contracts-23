0e1ac9f71b2c1c4c82a77a5b1c2f6f2ae36f04aa
Cloudy_Trylastsky <146930018+Desnation12@users.noreply.github.com>
2023-12-11 01:02:47 +0400

    Update DriverFunctions.sol

--- DPS/DriverFunctions.sol
+++ DPS/DriverFunctions.sol
@@ -12,14 +12,15 @@ contract DriverFunctions is PoliceFunctions {
             keccak256(abi.encodePacked(DocSystem[_numberDriverDoc].numberDriverDoc)), "is inknown number doc"); // add to number for userMap
 
         require( // check true input validity in system
-            keccak256(abi.encodePacked(_validity)) == 
-            keccak256(abi.encodePacked(DocSystem[_numberDriverDoc].validity)),"this validity not true");
+            keccak256(abi.encodePacked(_validity)) == //check == _validity user doc for validity doc for system
+            keccak256(abi.encodePacked(DocSystem[_numberDriverDoc].validity)),"this validity not true");  
         //check index 1-3, and check true input category
             require(_index <= 3 && _index != 0,"enter the 1-3"); // check index true 1-3
-            require(DocSystem[_numberDriverDoc].categoryDriverDoc == CategoryStruct(_index),"this category not true");
-            require(DocSystem[_numberDriverDoc].statusDoc == false,"this document already taken");
-            DocSystem[_numberDriverDoc].statusDoc = true;
+            require(DocSystem[_numberDriverDoc].categoryDriverDoc == CategoryStruct(_index),"this category not true"); // check category doc
+            require(DocSystem[_numberDriverDoc].statusDoc == false,"this document already taken"); // check status doc
+            DocSystem[_numberDriverDoc].statusDoc = true; // status document 
             userMap[msg.sender].DriverDoc = DocSystem[_numberDriverDoc];//add info for docs for userMap
+            userDocNumber[userMap[msg.sender].DriverDoc.numberDriverDoc] = msg.sender ; // add for mapping info/ number user docs = address user
     }
         
     function addTs(uint _index, uint _tsPrice, uint _lifeTime) public notCompany onlyReg { // add vehicle for user
@@ -44,32 +45,64 @@ contract DriverFunctions is PoliceFunctions {
         }
     }
 
-       
+    function extendDriveDoc(string memory _string) public notCompany onlyReg {//function for extend user driver doc
+        userMap[msg.sender].DriverDoc.validity = _string;// add string validity for docs user
+    }
+
+    function removeFine(uint _index) internal { // internal def for del payed fine
+        for(uint i = _index; i < userFines[msg.sender].length - 1; i++) {
+            userFines[msg.sender][_index].indexFine = userFines[msg.sender][_index + 1].indexFine;
+        }
+        userFines[msg.sender].pop();
+    }
+
+    function payFine(uint _index, uint _value) public notCompany onlyReg { // def for pay user fines
+        require(userMap[msg.sender].scoreUnpaidFines != 0,"you not have fines"); // check beacouse check
+        uint _time = block.timestamp; // start _time block.timestamp
+        if(userFines[msg.sender][_index].timeFine > _time){ // if time payed fine <= 5 min, price == 5 ether
+            if(userFines[msg.sender][_index].priceFine == _value) { // check user balance(token) == price fine
+                _transfer(msg.sender, bankAdr, _value); //  transfer user for bank fine sum price 
+                removeFine(_index); // delete payed fine 
+                userMap[msg.sender].scoreUnpaidFines--; // user score unpaid fines - 1
+            } 
+            else{
+                revert("not money, or your input money > price for you fine(5$)");
+           }
+        }
+        if(userFines[msg.sender][_index].timeFine < _time) { // if fine payed > 5 min fine price == 10 tokens
+            userFines[msg.sender][_index].priceFine = 10; // price fine == 10 tokens
+            if(userFines[msg.sender][_index].priceFine == _value) {// check user balance(token) == price fine
+                _transfer(msg.sender, bankAdr, _value); // transfer user for bank fine sum price 
+                removeFine(_index); // delete payed fine 
+                userMap[msg.sender].scoreUnpaidFines--; // user score unpaid fines - 1 
+            }
+            else{ // else return error "not money"
+                revert("not money"); // user balance(token) != fine price 
+            }      
+        }
+    }
 
     function _PriceIns() internal notCompany onlyReg { // internal function formula pricefor insuranse service
-        userMap[msg.sender].InsuranseFee = //formula
+        require(PriceForIns[msg.sender] == 0,"your price is set,payed your ins"); // check == 0 map user
+        PriceForIns[msg.sender] = //formula
         userTransport[msg.sender].marketPrice 
         * userTransport[msg.sender].lifetime/10 
         + 2*userMap[msg.sender].scoreUnpaidFines 
         + userMap[msg.sender].scoreDTP 
         - 2 * userMap[msg.sender].experience;
-    }
+        }
 
-    // function extendDriveDoc() public notCompany onlyReg {//function for extend user driver doc
-    //     userMap[msg.sender].validity[] + 10;
-    // }
-
-    // function extendYourIns(uint _value) public notCompany onlyReg { // ins service on function
-    //     require(_value >= userMap[msg.sender].InsuranseFee,"insufficient funds");// check sended money
-        
-    // } 
-
-    // function payFine(uint _index) public payable notCompany onlyReg {
-    //     uint _time = block.timestamp;
-    //     if(userFines[msg.sender][_index].timeFine > _time) {
-    //         if(userFines[msg.sender][_index].priceFine == msg.value) {
-    //         }
-    //     }
-    // } 
+     function extendYourIns(uint _value) public notCompany onlyReg { // ins service on function
+        require(InsStatusMap[msg.sender] != true,"your ins active!"); // sheck true for user ins
+        _PriceIns(); // calculate the price for insurance
+        _transfer(msg.sender, InsuranceCompanyAdr, PriceForIns[msg.sender]); // transfer for ins company tokens
+        InsStatusMap[msg.sender] = true; // status ins true
+        userMap[msg.sender].InsuranseFee = PriceForIns[msg.sender]; // insuranse Fee = price for ins map
+        PriceForIns[msg.sender] = 0; // map zeroing out
+        if(debtIns[InsuranceCompanyAdr] <= balanceOf(InsuranceCompanyAdr)) { // if balance ins company == balance ins company /
+            _transfer(InsuranceCompanyAdr, bankAdr, debtIns[InsuranceCompanyAdr]); // transfer for ims company debts summe
+            debtIns[InsuranceCompanyAdr] == 0; // debt for bank = 0
+        }  
+    } 
 }
 

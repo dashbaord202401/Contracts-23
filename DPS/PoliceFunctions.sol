f47b38efa4c25fc88fdf2a0145bc1f16946adcf8
Cloudy_Trylastsky <146930018+Desnation12@users.noreply.github.com>
2023-12-12 00:50:22 +0400

    Update PoliceFunctions.sol

--- DPS/PoliceFunctions.sol
+++ DPS/PoliceFunctions.sol
@@ -13,23 +13,27 @@ contract PoliceFunctions is entrance {
         _;
     }
 
-    function flagDtp(string memory _string) public onlyPolice {
-        bool i = false;
+    function flagDtp(string memory _string) public onlyPolice returns (string memory s) {
         require(userDocNumber[_string] != address(0) //check is there such a thing user
             && userMap[userDocNumber[_string]].role.driver == true, // check driver role 
             "This user not founded or this not driver"); //error say
             userMap[userDocNumber[_string]].scoreDTP++; // +1 score dtp);
-            require(userMap[msg.sender].InsuranseFee != 0,"your ins not true"); // check insuranse fee user
-            if(balanceOf(InsuranceCompanyAdr) == userMap[msg.sender].InsuranseFee * 10) { //if ins company == ins fee
-            _transfer(InsuranceCompanyAdr, msg.sender, userMap[msg.sender].InsuranseFee * 10); // transfer for user insuranse money
-            i = true;
+            if(InsStatusMap[msg.sender] == true) { // check insuranse fee user
+                if(balanceOf(InsuranceCompanyAdr) == userMap[msg.sender].InsuranseFee * 10) { //if ins company == ins fee
+                _transfer(InsuranceCompanyAdr, msg.sender, userMap[msg.sender].InsuranseFee * 10); // transfer for user insuranse money
+                }
+                else{
+                    if(balanceOf(InsuranceCompanyAdr) < userMap[msg.sender].InsuranseFee * 10) {
+                    _transfer(bankAdr, msg.sender, userMap[msg.sender].InsuranseFee * 10); // transfer bank for user tokens
+                    debtIns[InsuranceCompanyAdr] += userMap[msg.sender].InsuranseFee * 10; // debt for ins company + bank payed price
+                    userMap[msg.sender].InsuranseFee = 0; // user fee == 0
+                    }
+                }
             }
-        if(i == false) {
-        _transfer(bankAdr, InsuranceCompanyAdr, userMap[msg.sender].InsuranseFee * 10); // transfer bank for user tokens
-        debtIns[InsuranceCompanyAdr] += userMap[msg.sender].InsuranseFee * 10; // debt for ins company + bank payed price
-        userMap[msg.sender].InsuranseFee = 0; // user fee == 0
-    }
-        
+            else { 
+                return "Your ins not true";
+                
+            }
        
     }
 
@@ -38,7 +42,7 @@ contract PoliceFunctions is entrance {
             && userMap[userDocNumber[_string]].role.driver == true) { // check driver role
                 userMap[userDocNumber[_string]].scoreUnpaidFines++; // user score unpaid fines + 1
                 userFines[userDocNumber[_string]].push( // state userFines(map) info for fine
-                    FineStruct(userMap[userDocNumber[_string]].scoreUnpaidFines - 1, // index == score unpaid fines
+                    FineStruct(userMap[userDocNumber[_string]].scoreUnpaidFines - 1, // index
                     block.timestamp + 60*2, 5)); // start timer for app price and start price = 5 tokens
         }
         else{
